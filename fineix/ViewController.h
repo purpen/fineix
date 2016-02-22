5956b13bc669a6f28da1e7bf8a50b67252b1d043
purpen <purpen.w@gmail.com>
2016-02-22 09:39:28 +0800

    init code

---
+++ fineix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fineix
+//
+//  Created by xiaoyi on 16/2/22.
+//  Copyright © 2016年 taihuoniao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
