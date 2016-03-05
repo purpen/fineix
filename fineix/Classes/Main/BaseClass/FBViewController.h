561510406ff3397e3901cafe3ef09dbfdaf64e52
Fynn <y809204367@163.com>
2016-03-05 17:32:55 +0800

    修改主控制器的基类

---
+++ fineix/Classes/Main/BaseClass/FBViewController.h
@@ -0,0 +1,13 @@
+//
+//  FBViewController.h
+//  fineix
+//
+//  Created by FLYang on 16/3/5.
+//  Copyright © 2016年 taihuoniao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FBViewController : UIViewController
+
+@end
--- fineix/Classes/Main/My/Controller/MyViewController.h
+++ fineix/Classes/Main/My/Controller/MyViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "FBViewController.h"
 
-@interface MyViewController : UIViewController
+@interface MyViewController : FBViewController
 
 @end
