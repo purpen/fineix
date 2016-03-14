4c59d3fbd51efd488e24a336798d4a89e12facfd
THN-Dong <dongyongshengjj@sina.com>
2016-03-14 17:08:08 +0800

    no message

--- fineix/Classes/Main/Other/FBSearchLocationViewController.m
+++ fineix/Classes/Main/Other/FBSearchLocationViewController.m
@@ -66,16 +66,12 @@
     //设置地理位置搜索对象代理
     self.codeSearch.delegate = self;
     
-    _hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
-    _hud.mode = MBProgressHUDModeIndeterminate;
-    _hud.labelText = @"Loading";
-    //开始定位
-    [self startLocation];
+    
     self.tableV.mj_footer = [MJRefreshAutoNormalFooter footerWithRefreshingBlock:^{
         
         NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
         AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
-        NSString *urlString = [NSString stringWithFormat:@"http://api.map.baidu.com/place/v2/search?ak=20j0YXqCPWGqsPMTIcbF9Pv8&output=json&query=%@&page_size=10&page_num=%d&scope=1&region=%@",_address,m,@"全国"];
+        NSString *urlString = [NSString stringWithFormat:@"http://api.map.baidu.com/place/v2/search?ak=yO2KX6h4Gy5Tv4MYNrZFZLzN&output=json&query=%@&page_size=10&page_num=%d&scope=1&region=%@",_address,m,@"全国"];
         m = m + 1;
         NSString * urlstr = [urlString stringByAddingPercentEscapesUsingEncoding: NSUTF8StringEncoding];
         NSURL * URL = [NSURL URLWithString:urlstr];
@@ -99,6 +95,15 @@
         
     }];
     
+}
+
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    _hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    _hud.mode = MBProgressHUDModeIndeterminate;
+    _hud.labelText = @"Loading";
+    //开始定位
+    [self startLocation];
 }
 
 -(void)getDataList2:(id)sender{
@@ -188,7 +193,7 @@
             _address = result.address;
             NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
             AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
-            NSString *urlString = [NSString stringWithFormat:@"http://api.map.baidu.com/place/v2/search?ak=20j0YXqCPWGqsPMTIcbF9Pv8&output=json&query=%@&page_size=10&page_num=0&scope=1&region=%@",result.address,@"全国"];
+            NSString *urlString = [NSString stringWithFormat:@"http://api.map.baidu.com/place/v2/search?ak=yO2KX6h4Gy5Tv4MYNrZFZLzN&output=json&query=%@&page_size=10&page_num=0&scope=1&region=%@",result.address,@"全国"];
             
             NSString * urlstr = [urlString stringByAddingPercentEscapesUsingEncoding: NSUTF8StringEncoding];
             NSURL * URL = [NSURL URLWithString:urlstr];
@@ -307,7 +312,7 @@
         //在此处理正常结果
         NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
         AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
-        NSString *urlString = [NSString stringWithFormat:@"http://api.map.baidu.com/place/v2/suggestion?query=%@&region=131&output=json&ak=20j0YXqCPWGqsPMTIcbF9Pv8",_searchTF.text];
+        NSString *urlString = [NSString stringWithFormat:@"http://api.map.baidu.com/place/v2/suggestion?query=%@&region=131&output=json&ak=yO2KX6h4Gy5Tv4MYNrZFZLzN",_searchTF.text];
         
         NSString * urlstr = [urlString stringByAddingPercentEscapesUsingEncoding: NSUTF8StringEncoding];
         NSURL * URL = [NSURL URLWithString:urlstr];
