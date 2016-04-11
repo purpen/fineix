06424d021c80e8edfd401bc52b7a054e6e755d13
THN-Dong <dongyongshengjj@sina.com>
2016-04-11 19:44:59 +0800

    完善个人信息

---
+++ Fiu/Classes/Main/LoginRegister/controller/SubscribeInterestedViewController.h
@@ -0,0 +1,13 @@
+//
+//  SubscribeInterestedViewController.h
+//  Fiu
+//
+//  Created by THN-Dong on 16/4/11.
+//  Copyright © 2016年 taihuoniao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SubscribeInterestedViewController : UIViewController
+
+@end
---
+++ Fiu/Classes/Main/LoginRegister/controller/SubscribeInterestedViewController.m
@@ -0,0 +1,37 @@
+//
+//  SubscribeInterestedViewController.m
+//  Fiu
+//
+//  Created by THN-Dong on 16/4/11.
+//  Copyright © 2016年 taihuoniao. All rights reserved.
+//
+
+#import "SubscribeInterestedViewController.h"
+
+@interface SubscribeInterestedViewController ()
+
+@end
+
+@implementation SubscribeInterestedViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
