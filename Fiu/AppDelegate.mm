9eaeaab94d0a67482bc979717bb642c6e83195cc
THN-Dong <dongyongshengjj@sina.com>
2016-07-12 18:23:01 +0800

    no message

--- Fiu/AppDelegate.mm
+++ Fiu/AppDelegate.mm
@@ -293,6 +293,27 @@ NSString *const determineLogin = @"/auth/check_login";
     return YES;
 }
 
+-(BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options{
+    if ([UMSocialSnsService handleOpenURL:url]) {
+        return YES;
+    }
+    if ([WXApi handleOpenURL:url delegate:self]) {
+        return YES;
+    }
+    
+    //如果极简开发包不可用，会跳转支付宝钱包进行支付，需要将支付宝钱包的支付结果回传给开发包
+    if ([url.host isEqualToString:@"safepay"]) {
+        [[AlipaySDK defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic) {
+            //【由于在跳转支付宝客户端支付的过程中，商户app在后台很可能被系统kill了，所以pay接口的callback就会失效，请商户对standbyCallback返回的回调结果进行处理,就是在这个方法里面处理跟callback一样的逻辑】
+            if (_aliDelegate && [_aliDelegate respondsToSelector:@selector(standbyCallbackWithResultDic:)]) {
+                [_aliDelegate standbyCallbackWithResultDic:resultDic];
+            }
+        }];
+    }
+    
+    return YES;
+}
+
 -(BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation{
     if ([UMSocialSnsService handleOpenURL:url]) {
         return YES;
