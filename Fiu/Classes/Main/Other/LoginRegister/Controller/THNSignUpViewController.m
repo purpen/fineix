b98715f4cf2ed0354d421b603ace7d876cb8f4bb
THN-Dong <609042238@qq.com>
2016-08-10 20:31:04 +0800

    no message

--- Fiu/Classes/Main/Other/LoginRegister/Controller/THNSignUpViewController.m
+++ Fiu/Classes/Main/Other/LoginRegister/Controller/THNSignUpViewController.m
@@ -8,7 +8,7 @@
 
 #import "THNSignUpViewController.h"
 
-@interface THNSignUpViewController ()
+@interface THNSignUpViewController ()<UITextFieldDelegate>
 
 /**  */
 @property (nonatomic, strong) NSMutableArray *textAry;
@@ -19,13 +19,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    for (int i = 0; i < 11; i++) {
-        UITextField *tF = [[UITextField alloc] initWithFrame:CGRectMake(80 + i * 5, 220, 2, 20)];
-        tF.placeholder = @"*";
-        tF.borderStyle = UITextBorderStyleNone;
-        tF.textColor = [UIColor whiteColor];
-        [self.view addSubview:tF];
-    }
+    
 }
 
 
