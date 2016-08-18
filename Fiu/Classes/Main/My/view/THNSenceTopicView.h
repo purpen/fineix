b2f35a545c396b86e549b064f2efae350a38921b
Your Name <you@example.com>
2016-08-18 23:59:25 +0800

    推荐的情境主题样式修改

---
+++ Fiu/Classes/Main/My/view/THNSenceTopicView.h
@@ -0,0 +1,13 @@
+//
+//  THNSenceTopicView.h
+//  Fiu
+//
+//  Created by dys on 16/8/18.
+//  Copyright © 2016年 taihuoniao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface THNSenceTopicView : UIView
+
+@end
---
+++ Fiu/Classes/Main/My/view/THNSenceTopicView.m
@@ -0,0 +1,21 @@
+//
+//  THNSenceTopicView.m
+//  Fiu
+//
+//  Created by dys on 16/8/18.
+//  Copyright © 2016年 taihuoniao. All rights reserved.
+//
+
+#import "THNSenceTopicView.h"
+
+@implementation THNSenceTopicView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
