141bf62b6b15f31c1b74d2f56fcba9e1f96e1c34
Fynn <y809204367@163.com>
2016-09-22 14:59:53 +0800

    添加送红包视图

---
+++ Fiu/Expand/Category/THNRedEnvelopeView.h
@@ -0,0 +1,13 @@
+//
+//  THNRedEnvelopeView.h
+//  Fiu
+//
+//  Created by FLYang on 16/9/22.
+//  Copyright © 2016年 taihuoniao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface THNRedEnvelopeView : UIView
+
+@end
---
+++ Fiu/Expand/Category/THNRedEnvelopeView.m
@@ -0,0 +1,21 @@
+//
+//  THNRedEnvelopeView.m
+//  Fiu
+//
+//  Created by FLYang on 16/9/22.
+//  Copyright © 2016年 taihuoniao. All rights reserved.
+//
+
+#import "THNRedEnvelopeView.h"
+
+@implementation THNRedEnvelopeView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
