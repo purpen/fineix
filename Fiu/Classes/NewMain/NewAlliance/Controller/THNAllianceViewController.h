437a60464680152125be2f8d6598a7ba749d0970
Fynn <y809204367@163.com>
2017-01-13 18:28:19 +0800

    添加太火鸟联盟分成目录

---
+++ Fiu/Classes/NewMain/NewAlliance/Controller/THNAllianceViewController.h
@@ -0,0 +1,13 @@
+//
+//  THNAllianceViewController.h
+//  Fiu
+//
+//  Created by FLYang on 2017/1/13.
+//  Copyright © 2017年 taihuoniao. All rights reserved.
+//
+
+#import "THNViewController.h"
+
+@interface THNAllianceViewController : THNViewController
+
+@end
---
+++ Fiu/Classes/NewMain/NewAlliance/Controller/THNAllianceViewController.m
@@ -0,0 +1,24 @@
+//
+//  THNAllianceViewController.m
+//  Fiu
+//
+//  Created by FLYang on 2017/1/13.
+//  Copyright © 2017年 taihuoniao. All rights reserved.
+//
+
+#import "THNAllianceViewController.h"
+
+@interface THNAllianceViewController ()
+
+@end
+
+@implementation THNAllianceViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+
+@end
