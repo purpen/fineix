42abe06ca9230c1312140e608c002f4685390bd9
THN-Dong <609042238@qq.com>
2017-03-08 18:30:06 +0800

    完善支付宝账户信息

--- Fiu/Classes/NewMain/NewAlliance/Model/THNZhangHuModel.h
+++ Fiu/Classes/NewMain/NewAlliance/Model/THNZhangHuModel.h
@@ -15,5 +15,18 @@
 @property(nonatomic,copy) NSString *phone;
 @property(nonatomic,copy) NSString *pay_type_label;
 @property(nonatomic,copy) NSString *username;
+@property(nonatomic,copy) NSString *user_id;
+@property(nonatomic,copy) NSString *alliance;
+@property(nonatomic,copy) NSString *type;
+@property(nonatomic,copy) NSString *kind;
+@property(nonatomic,copy) NSString *kind_label;
+@property(nonatomic,copy) NSString *pay_type;
+@property(nonatomic,copy) NSString *account;
+@property(nonatomic,copy) NSString *is_default;
+@property(nonatomic,copy) NSString *bank_address;
+@property(nonatomic,copy) NSString *status;
+@property(nonatomic,copy) NSString *created_on;
+@property(nonatomic,copy) NSString *updated_on;
+@property(nonatomic,copy) NSString *created_at;
 
 @end
