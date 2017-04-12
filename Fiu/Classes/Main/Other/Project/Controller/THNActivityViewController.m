0d7ad6d5e56a3c1af7e26ebb4f4d2122252e8728
dong <609042238@qq.com>
2017-04-12 12:01:20 +0800

    关注和粉丝页修改用户id未对应正确用户的问题。

--- Fiu/Classes/Main/Other/Project/Controller/THNActivityViewController.m
+++ Fiu/Classes/Main/Other/Project/Controller/THNActivityViewController.m
@@ -75,7 +75,6 @@ static NSString *const cellId = @"THNActivityCollectionViewCell";
     FBRequest *request = [FBAPI postWithUrlString:@"/scene_subject/getlist" requestDictionary:params delegate:self];
     [request startRequestSuccess:^(FBRequest *request, id result) {
         if (result[@"success"]) {
-            NSLog(@"活动 %@",result);
             self.current_page = [result[@"data"][@"current_page"] integerValue];
             self.total_rows = [result[@"data"][@"total_rows"] integerValue];
             NSArray *rows = result[@"data"][@"rows"];
